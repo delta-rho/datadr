31f690e57c43b5ea9d816a36c7c0d0a3ced4d77d
hafen <rhafen@gmail.com>
2016-02-26 14:58:17 -0800

    Remove Spark support for now

--- R/ddo_ddf_kvSpark.R
+++ R/ddo_ddf_kvSpark.R
@@ -1,153 +1,155 @@
-## Methods for object of class "kvSparkData" - key-value pairs as Spark RDDs
+NULL
 
-#' @export
-ddoInit.sparkDataConn <- function(obj, ...) {
-  structure(list(), class = "kvSparkData")
-}
+# ## Methods for object of class "kvSparkData" - key-value pairs as Spark RDDs
 
-#' @export
-ddoInitConn.sparkDataConn <- function(obj, ...) {
-  if(obj$hdfs) {
-    paths <- paste(obj$hdfsURI, rhls(obj$loc, recurse = TRUE)$file, sep = "")
-    regxp <- rhoptions()$file.types.remove.regex
-  } else {
-    paths <- paths[!grepl(rhoptions()$file.types.remove.regex, paths)]
-    regxp <- "(/_meta|/_outputs|/_SUCCESS|/_LOG|/_log)"
-  }
-  paths <- paths[!grepl(regxp, paths)]
-  if(length(paths) == 0)
-    stop("No data found - use addData() or specify a connection with a location that contains data.")
-  obj$data <- objectFile(getSparkContext(), paths)
-  obj
-}
+# #' @export
+# ddoInit.sparkDataConn <- function(obj, ...) {
+#   structure(list(), class = "kvSparkData")
+# }
 
-#' @export
-requiredObjAttrs.kvSparkData <- function(obj) {
-  list(
-    ddo = getDr("requiredDdoAttrs"),
-    ddf = getDr("requiredDdfAttrs")
-  )
-}
+# #' @export
+# ddoInitConn.sparkDataConn <- function(obj, ...) {
+#   if(obj$hdfs) {
+#     paths <- paste(obj$hdfsURI, rhls(obj$loc, recurse = TRUE)$file, sep = "")
+#     regxp <- rhoptions()$file.types.remove.regex
+#   } else {
+#     paths <- paths[!grepl(rhoptions()$file.types.remove.regex, paths)]
+#     regxp <- "(/_meta|/_outputs|/_SUCCESS|/_LOG|/_log)"
+#   }
+#   paths <- paths[!grepl(regxp, paths)]
+#   if(length(paths) == 0)
+#     stop("No data found - use addData() or specify a connection with a location that contains data.")
+#   obj$data <- objectFile(getSparkContext(), paths)
+#   obj
+# }
 
-#' @export
-getBasicDdoAttrs.kvSparkData <- function(obj, conn) {
-  if(conn$hdfs) {
-    ff <- rhls(conn$loc, recurse = TRUE)
-    ff <- ff[!grepl("\\/_meta", ff$file),]
-    sz <- sum(ff$size)
-  } else {
-    ff <- list.files(conn$loc, recursive = TRUE)
-    ff <- ff[!grepl("_meta\\/", ff)]
-    sz <- sum(file.info(file.path(conn$loc, ff))$size)
-  }
+# #' @export
+# requiredObjAttrs.kvSparkData <- function(obj) {
+#   list(
+#     ddo = getDr("requiredDdoAttrs"),
+#     ddf = getDr("requiredDdfAttrs")
+#   )
+# }
 
-  ex <- take(conn$data, 1)[[1]]
-  if(conn$type == "text")
-    ex <- list("", ex)
+# #' @export
+# getBasicDdoAttrs.kvSparkData <- function(obj, conn) {
+#   if(conn$hdfs) {
+#     ff <- rhls(conn$loc, recurse = TRUE)
+#     ff <- ff[!grepl("\\/_meta", ff$file),]
+#     sz <- sum(ff$size)
+#   } else {
+#     ff <- list.files(conn$loc, recursive = TRUE)
+#     ff <- ff[!grepl("_meta\\/", ff)]
+#     sz <- sum(file.info(file.path(conn$loc, ff))$size)
+#   }
 
-  list(
-    conn = conn,
-    extractableKV = TRUE,
-    totStorageSize = sz,
-    totObjectSize = NA,
-    nDiv = NA, # length(conn$data),
-    example = ex
-  )
-}
+#   ex <- take(conn$data, 1)[[1]]
+#   if(conn$type == "text")
+#     ex <- list("", ex)
 
-#' @export
-getBasicDdfAttrs.kvSparkData <- function(obj) {
-  list(vars = lapply(kvExample(obj)[[2]], class))
-}
+#   list(
+#     conn = conn,
+#     extractableKV = TRUE,
+#     totStorageSize = sz,
+#     totObjectSize = NA,
+#     nDiv = NA, # length(conn$data),
+#     example = ex
+#   )
+# }
 
-# kvSparkData is never extractable (yet...)
-#' @export
-hasExtractableKV.kvSparkData <- function(x) {
-  FALSE
-}
+# #' @export
+# getBasicDdfAttrs.kvSparkData <- function(obj) {
+#   list(vars = lapply(kvExample(obj)[[2]], class))
+# }
 
-######################################################################
-### extract methods
-######################################################################
+# # kvSparkData is never extractable (yet...)
+# #' @export
+# hasExtractableKV.kvSparkData <- function(x) {
+#   FALSE
+# }
 
-#' @export
-extract.kvSparkData <- function(x, i, ...) {
-  idx <- NULL
+# ######################################################################
+# ### extract methods
+# ######################################################################
 
-  dat <- getAttribute(x, "conn")$data
-  keys <- getKeys(x)
+# #' @export
+# extract.kvSparkData <- function(x, i, ...) {
+#   idx <- NULL
 
-  if(is.numeric(i)) {
-    idx <- i
-    if(length(idx) == 1)
-      return(take(dat, 1))
-  } else {
-    keyHashes <- getAttribute(x, "keyHashes")
+#   dat <- getAttribute(x, "conn")$data
+#   keys <- getKeys(x)
 
-    # try actual key
-    idx <- unlist(lapply(as.character(sapply(i, digest)), function(x) which(keyHashes == x)))
+#   if(is.numeric(i)) {
+#     idx <- i
+#     if(length(idx) == 1)
+#       return(take(dat, 1))
+#   } else {
+#     keyHashes <- getAttribute(x, "keyHashes")
 
-    if(length(idx) == 0 && is.character(i)) {
-      if(all(nchar(i) == 32)) {
-        idx <- unlist(lapply(i, function(x) which(keyHashes == x)))
-      }
-    }
-  }
+#     # try actual key
+#     idx <- unlist(lapply(as.character(sapply(i, digest)), function(x) which(keyHashes == x)))
 
-  if(length(idx) == 0)
-    return(NULL)
+#     if(length(idx) == 0 && is.character(i)) {
+#       if(all(nchar(i) == 32)) {
+#         idx <- unlist(lapply(i, function(x) which(keyHashes == x)))
+#       }
+#     }
+#   }
 
-  lapply(idx, function(a) {
-    lookup(dat, keys[[a]])
-  })
-}
+#   if(length(idx) == 0)
+#     return(NULL)
 
-######################################################################
-### convert methods
-######################################################################
+#   lapply(idx, function(a) {
+#     lookup(dat, keys[[a]])
+#   })
+# }
 
-#' @export
-convertImplemented.kvSparkData <- function(obj) {
-  c("sparkDataConn", "NULL")
-}
+# ######################################################################
+# ### convert methods
+# ######################################################################
 
-#' @export
-convert.kvSparkData <- function(from, to = NULL, overwrite = FALSE) {
-  convertkvSparkData(to, from)
-}
+# #' @export
+# convertImplemented.kvSparkData <- function(obj) {
+#   c("sparkDataConn", "NULL")
+# }
 
-convertkvSparkData <- function(obj, ...)
-  UseMethod("convertkvSparkData", obj)
+# #' @export
+# convert.kvSparkData <- function(from, to = NULL, overwrite = FALSE) {
+#   convertkvSparkData(to, from)
+# }
 
-# from sparkData to sparkData
-#' @export
-convertkvSparkData.sparkDataConn <- function(to, from, verbose = FALSE) {
-  from
-}
+# convertkvSparkData <- function(obj, ...)
+#   UseMethod("convertkvSparkData", obj)
 
-# from sparkData to memory
-#' @export
-convertkvSparkData.NULL <- function(to, from, verbose = FALSE) {
-  res <- collect(getAttribute(from, "conn")$data)
-
-  if(inherits(from, "ddf")) {
-    res <- ddf(res, update = FALSE, verbose = verbose)
-  } else {
-    res <- ddo(res, update = FALSE, verbose = verbose)
-  }
-
-  addNeededAttrs(res, from)
-}
-
-
-# # from sparkData to local disk
+# # from sparkData to sparkData
 # #' @export
 # convertkvSparkData.sparkDataConn <- function(to, from, verbose = FALSE) {
 #   from
 # }
-#
-# # from sparkData to HDFS
+
+# # from sparkData to memory
 # #' @export
-# convertkvSparkData.hdfsConn <- function(to, from, verbose = FALSE) {
+# convertkvSparkData.NULL <- function(to, from, verbose = FALSE) {
+#   res <- collect(getAttribute(from, "conn")$data)
+
+#   if(inherits(from, "ddf")) {
+#     res <- ddf(res, update = FALSE, verbose = verbose)
+#   } else {
+#     res <- ddo(res, update = FALSE, verbose = verbose)
+#   }
+
+#   addNeededAttrs(res, from)
 # }
-#
+
+
+# # # from sparkData to local disk
+# # #' @export
+# # convertkvSparkData.sparkDataConn <- function(to, from, verbose = FALSE) {
+# #   from
+# # }
+# #
+# # # from sparkData to HDFS
+# # #' @export
+# # convertkvSparkData.hdfsConn <- function(to, from, verbose = FALSE) {
+# # }
+# #
